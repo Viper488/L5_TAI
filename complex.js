8f7588ff6cc72774d796aaf6e07316aff42c670b
Artur Hamernik <hamernik18@gmail.com>
2021-03-22 09:02:32 +0100

    tsconfig

--- complex.js
+++ complex.js
@@ -1,27 +1,27 @@
-var Complex = /** @class */ (function () {
-    function Complex(real, imaginary) {
+"use strict";
+class Complex {
+    constructor(real, imaginary) {
         this.real = real;
         this.imaginary = imaginary;
     }
-    Complex.prototype.add = function (obj) {
+    add(obj) {
         return new Complex(this.real + obj.real, this.imaginary + obj.imaginary);
-    };
-    Complex.prototype.subtract = function (obj) {
+    }
+    subtract(obj) {
         return new Complex(this.real - obj.real, this.imaginary - obj.imaginary);
-    };
-    Complex.prototype.modulo = function () {
+    }
+    modulo() {
         return Math.sqrt(this.real * this.real + this.imaginary * this.imaginary);
-    };
-    Complex.prototype.to_string = function () {
+    }
+    to_string() {
         if (this.imaginary > 0) {
             return this.real + " + " + this.imaginary + "i";
         }
         else {
             return this.real + " " + this.imaginary + "i";
         }
-    };
-    return Complex;
-}());
+    }
+}
 var number1 = new Complex(3, 4);
 var number2 = new Complex(2, 5);
 var add = number1.add(number2);
